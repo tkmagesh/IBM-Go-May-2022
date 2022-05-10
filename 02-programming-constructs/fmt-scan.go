1a0c686bcaa3096294e59fd05e124f2875ed6519
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-10 15:55:41 +0530

    using the fmt.Scan functions to accept input from the user

---
+++ 02-programming-constructs/fmt-scan.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	fmt.Println("Enter the name :")
+	fmt.Scanln(&name)
+	fmt.Printf("Hi %s, Have a nice day!\n", name)
+
+	var n1, n2 int
+	fmt.Println("Enter two numbers")
+	//fmt.Scanln(&n1, &n2)
+	fmt.Scanf("%d-%d\n", &n1, &n2)
+	fmt.Println(n1, n2)
+}
