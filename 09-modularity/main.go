ab048ab6704281beff5f2fc66bc4e9bce37c8682
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-11 15:13:27 +0530

    using third party packages

--- 09-modularity/main.go
+++ 09-modularity/main.go
@@ -5,6 +5,8 @@ import (
 	"modularity-demo/calc"
 	calcUtils "modularity-demo/calc/utils"
 	_ "modularity-demo/dummy"
+
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -12,4 +14,6 @@ func main() {
 	fmt.Println(calc.Subtract(100, 200))
 	fmt.Printf("OpCount = %d\n", calc.OpCount())
 	fmt.Println("Is 21 an even number ? :", calcUtils.IsEven(21))
+	color.Red("This text will be printed in red color")
+	color.Blue("This text will be printed in blue color")
 }
