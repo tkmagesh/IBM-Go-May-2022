fe5b5ab56a66c2d54376d772251af5fbd1b85a94
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-12 12:08:29 +0530

    interfaces

---
+++ 12-interfaces/program.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	Radius float32
+}
+
+func (c Circle) Area() float32 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float32 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Rectangle struct {
+	Height float32
+	Width  float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float32 {
+	return 2 * (r.Height + r.Width)
+}
+
+type ShapeWithArea interface {
+	Area() float32
+}
+
+func printArea(sa ShapeWithArea) {
+	fmt.Println("Area = ", sa.Area())
+}
+
+type ShapeWithPerimeter interface {
+	Perimeter() float32
+}
+
+func printPerimeter(sp ShapeWithPerimeter) {
+	fmt.Println("Perimeter = ", sp.Perimeter())
+}
+
+type Shape interface {
+	ShapeWithArea
+	ShapeWithPerimeter
+}
+
+func printShape(s Shape) {
+	printArea(s)
+	printPerimeter(s)
+}
+
+func main() {
+	c := Circle{Radius: 12}
+	/*
+		printArea(c)
+		printPerimeter(c)
+	*/
+	printShape(c)
+	r := Rectangle{Height: 10, Width: 10}
+	/*
+		printArea(r)
+		printPerimeter(r)
+	*/
+	printShape(r)
+
+}
