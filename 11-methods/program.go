e84eab5d36d356b52c052730dfc36d9a34807d26
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-12 10:45:01 +0530

    creating methods for non local types using type alias

--- 11-methods/program.go
+++ 11-methods/program.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"methods-demo/models"
 )
+
+type MyStr string
+
+func (myStr MyStr) Length() int {
+	return len(myStr)
+}
 
 func main() {
 	e := models.Employee{Name: "Magesh"}
 	e.WhoAmI()
+
+	//type conversion
+	var i int = 100
+	var f float32
+	f = float32(i)
+	fmt.Println(f)
+
+	s := MyStr("This is a sample string")
+	fmt.Println(s.Length())
 }
