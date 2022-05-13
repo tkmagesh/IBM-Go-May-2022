1df2871bb7147c48b55516956a1a1b2981dd372a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-13 12:25:28 +0530

    using 'select case' construct

--- 14-concurrency/demo-14.go
+++ 14-concurrency/demo-14.go
@@ -9,19 +9,21 @@ func main() {
 	ch1 := f1()
 	ch2 := f2()
 
-	data1 := <-ch1
-	fmt.Println(data1)
-
-	data2 := <-ch2
-	fmt.Println(data2)
-
+	for i := 0; i < 2; i++ {
+		select {
+		case data1 := <-ch1:
+			fmt.Println(data1)
+		case data2 := <-ch2:
+			fmt.Println(data2)
+		}
+	}
 	fmt.Println("Done")
 }
 
 func f1() chan string {
 	ch := make(chan string)
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(5 * time.Second)
 		ch <- "f1 completed"
 	}()
 	return ch
@@ -30,7 +32,7 @@ func f1() chan string {
 func f2() chan string {
 	ch := make(chan string)
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(2 * time.Second)
 		ch <- "f2 completed"
 	}()
 	return ch
